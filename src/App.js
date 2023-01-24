837121c2f8252cb4da035e92392ff53ebee8bbbc
Pushparaj Singh <pushparajsingh448@gmail.com>
2023-01-24 11:40:35 +0530

    hello world

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Featureshow from "./component/featureshow";
+import Projectshow from "./component/projectshow";
+import Todolistshow from "./component/todolistshow";
+
+function App() {
+  const a = "hello world";
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Projectshow cmp={a} />} />
+        <Route path="/feature/:index" element={<Featureshow />} />
+        <Route path="/todolist/:index/:i" element={<Todolistshow />} />
+        <Route path="*" element={<>Page Not Found</>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
